1766512beb7d9666aeda271a3d44c4cf6a49412f
Michael Matloka <michael@matloka.com>
2026-01-04 15:34:10 +0100

    Add icon things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Head from "next/head";
 import { QueryProvider } from "@/components/QueryProvider";
 import { SessionProvider } from "@/components/SessionProvider";
 import { createClient } from "@/lib/supabase/server";
@@ -31,6 +32,23 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <meta name="apple-mobile-web-app-title" content="SketchViz" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <GoogleTagManager gtmScriptUrl="https://www.googletagmanager.com/gtag/js?id=AW-971292206" />
       <body
         className={`${outfit.variable} antialiased`}
