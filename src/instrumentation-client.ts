a2e8f7c3c199465321c1c0884ea6f09a7f91f372
Michael Matloka <michael@matloka.com>
2026-01-20 01:18:52 +0100

    Always create PostHog person profiles

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -7,4 +7,5 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
       : process.env.NEXT_PUBLIC_POSTHOG_HOST,
   ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   defaults: "2025-11-30",
+  person_profiles: "always",
 });
